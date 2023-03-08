1850998dd3a9f3502ee159ee35c9675c39e3ce2d
Dariusz Okoński <darek200180@gmail.com>
2023-03-08 22:49:27 +0100

    add menu

---
+++ client/src/components/Menu/Menu.tsx
@@ -0,0 +1,17 @@
+import './Menu.css';
+
+export default function Menu() {
+  return (
+    <ul className='Menu'>
+      <li>
+        <a href='#'>Developers</a>
+      </li>
+      <li>
+        <a href='#'>Register</a>
+      </li>
+      <li>
+        <a href='#'>Login</a>
+      </li>
+    </ul>
+  );
+}
--- client/src/components/Navigation/Navigation.tsx
+++ client/src/components/Navigation/Navigation.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import Logo from '../Logo/Logo';
+import Menu from '../Menu/Menu';
 import './Navigation.css';
 
 export default function Navigation() {
   return (
     <nav className='Navigation'>
       <Logo />
+      <Menu />
     </nav>
   );
 }
