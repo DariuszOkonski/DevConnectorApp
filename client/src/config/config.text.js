89729cd0a208246d9119af5d8d4f9ad119cccd51
Dariusz Okoński <darek200180@gmail.com>
2023-03-08 22:18:07 +0100

    add configText

---
+++ client/src/config/config.text.js
@@ -0,0 +1,11 @@
+export const configText = {
+  home: {
+    header: 'Developer Connector',
+    paragraph:
+      'Create a developer profile/portfolio, share posts and get help from other developers',
+  },
+  button: {
+    signUp: 'Sign Up',
+    Login: 'Login',
+  },
+};
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,19 +1,17 @@
 import React from 'react';
 import Button from '../../components/Button/Button';
+import { configText } from '../../config/config.text';
 import './Home.css';
 
 export default function Home() {
   return (
     <div className='Home'>
-      <h1 className='Home__header'>Developer Connector</h1>
-      <p className='Home__paragraph'>
-        Create a developer profile/portfolio, share posts and get help from
-        other developers
-      </p>
+      <h1 className='Home__header'>{configText.home.header}</h1>
+      <p className='Home__paragraph'>{configText.home.paragraph}</p>
 
       <div>
-        <Button text='Sign Up' secondaryButton />
-        <Button text='Login' />
+        <Button text={configText.button.signUp} secondaryButton />
+        <Button text={configText.button.Login} />
       </div>
     </div>
   );
