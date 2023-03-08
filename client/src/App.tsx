2bfdb4a1ca2171547946122a97bac46f0637fe6b
Dariusz Okoński <darek200180@gmail.com>
2023-03-08 21:20:02 +0100

    remove sass

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1 className='Header'>Header</h1>
+      <h2>Welcome</h2>
     </div>
   );
 }
