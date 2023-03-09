1502e0f045e3fa621e850a45530b0df57633b58b
Dariusz Okoński <darek200180@gmail.com>
2023-03-09 20:11:55 +0100

    add container

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Home from './pages/Home/Home';
+import './css/index.css';
 
 function App() {
   return (
